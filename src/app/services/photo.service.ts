178b0af29528fea553bf21d7eff7af714bf1050d
JonathanJairxd <jonathanjairxd02@gmail.com>
2025-04-28 18:07:10 -0500

    Guardar fotos en el sistema de archivos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import{ Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera'
+import { WebView } from '@capacitor/core';
 import {Filesystem, Directory} from '@capacitor/filesystem'
 import {Preferences} from '@capacitor/preferences'
 
@@ -8,15 +9,56 @@ import {Preferences} from '@capacitor/preferences'
 @Injectable({
   providedIn: 'root'
 })
-export class PhotoService {
 
+
+export class PhotoService {
+public photos:UserPhoto[]=[];
   constructor() { }
+  
+  private async savePicture(photo:Photo){
+    const base64Data= await this.readAsBase64(photo);
+    const fileName = Date.now() + '.jpeg';
+  const savedFile = await Filesystem.writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: Directory.Data
+  });
+
+  return {
+    filepath: fileName,
+    webviewPath: photo.webPath
+  };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
   public async addNewToGallery(){
-    const capturePhoto = await Camera.getPhoto({
+    const capturedPhoto=await Camera.getPhoto({
       resultType:CameraResultType.Uri,
       source:CameraSource.Camera,
       quality:100
-    })
-  }
+    });
+
+    const savedImageFile=await this.savePicture(capturedPhoto)
+     this.photos.unshift(savedImageFile)
+  } 
 }
 
+export interface UserPhoto{
+  filepath:string;
+  webviewPath?:string
+}
